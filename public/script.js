eb9f8201147162154426cf530e6b90563a6da748
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2019-12-17 20:57:15 +0530

    add animation to key on keyup event

--- public/script.js
+++ public/script.js
@@ -1,8 +1,19 @@
 window.addEventListener('keyup', (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   if (audio) {
     audio.currentTime = 0; // rewind to start
     audio.play();
+
+    key.classList.add('playing'); // animation
   }
 });
+
+const keys = document.querySelectorAll('.key');
+
+for (let i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('transitionend', () => {
+    keys[i].classList.remove('playing');
+  });
+}
